74899ff3264d24b808f54e692041903e125fe13e
Jagadesh-Ronanki <jagadesh.ronanki@gmail.com>
2025-05-31 12:52:48 +0530

    [+] Nextjs + Tailwindv4 + Shadcn + next-themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from "../../theme/theme-provider";
+import { ThemeToggle } from "../../theme/theme-toggle";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider>
+          {children}
+          <ThemeToggle />
+        </ThemeProvider>
       </body>
     </html>
   );
